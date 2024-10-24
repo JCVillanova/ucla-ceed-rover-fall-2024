66129772b4c1dc7f99b2c9fc19169ab1c5308fca
Joshua Villanova <joshuacvillanova@gmail.com>
2024-10-24 15:24:28 -0700

    Update test.cpp

--- Scripts/test.cpp/test.cpp.ino
+++ Scripts/test.cpp/test.cpp.ino
@@ -14,6 +14,27 @@
 #define rightBack_in4 12
 #define rightBack_in3 13
 
+// Ultrasonic sensor
+#define sonar1 22
+#define sonar2 23
+#define sonar3 24
+#define sonar4 25
+#define sonar5 26
+#define sonar6 27
+#define MAX_DISTANCE 200
+#define NUM_SONAR 6
+
+NewPing sonar[NUM_SONAR] = { // array of ultrasonic sensors
+  NewPing(sonar1, sonar1, MAX_DISTANCE),
+  NewPing(sonar2, sonar2, MAX_DISTANCE),
+  NewPing(sonar3, sonar3, MAX_DISTANCE),
+  NewPing(sonar4, sonar4, MAX_DISTANCE),
+  NewPing(sonar5, sonar5, MAX_DISTANCE),
+  NewPing(sonar6, sonar6, MAX_DISTANCE)
+};
+
+int distance[NUM_SONAR]; // distance array for each sensor
+
 // setup() method is called once when the program begins
 void setup() {
   // Configure Motor 1 pins as outputs
@@ -36,6 +57,123 @@ void setup() {
 // loop() method is called repeatedly as long as the program is running
 void loop() {
   testMovement(&goForward, 4000);
+}
+
+// Update distance array for all sensors
+void updateSonar() {
+  for(int i = 0; i < NUM_SONAR; ++i) {
+    distance[i] = sonar[i].ping_in();
+    if(distance[i] == 0) distance[i] = MAX_DISTANCE;
+  }
+}
+
+// All four wheels move forward
+void goForward() {
+  // Set alternating voltages on each wheel's pin
+  digitalWrite(leftFront_in4, LOW);
+  digitalWrite(leftFront_in3, HIGH);
+  digitalWrite(rightFront_in1, LOW);
+  digitalWrite(rightFront_in2, HIGH);
+  digitalWrite(leftBack_in1, LOW);
+  digitalWrite(leftBack_in2, HIGH);
+  digitalWrite(rightBack_in4, LOW);
+  digitalWrite(rightBack_in3, HIGH);
+
+  // Apply voltage to pins at previously specified voltages
+  analogWrite(leftFront_enB, 255);
+  analogWrite(rightFront_enA, 255);
+  analogWrite(leftBack_enA, 255);
+  analogWrite(rightBack_enB, 255);
+}
+
+// All four wheels move backward
+void goBackward() {
+  digitalWrite(leftFront_in4, HIGH);
+  digitalWrite(leftFront_in3, LOW);
+  digitalWrite(rightFront_in1, HIGH);
+  digitalWrite(rightFront_in2, LOW);
+  digitalWrite(leftBack_in1, HIGH);
+  digitalWrite(leftBack_in2, LOW);
+  digitalWrite(rightBack_in4, HIGH);
+  digitalWrite(rightBack_in3, LOW);
+
+  analogWrite(leftFront_enB, 255);
+  analogWrite(rightFront_enA, 255);
+  analogWrite(leftBack_enA, 255);
+  analogWrite(rightBack_enB, 255);
+}
+
+// Left front and right back wheels move forward, right front and left back wheels move backward
+void goRight() {
+  digitalWrite(leftFront_in4, LOW);
+  digitalWrite(leftFront_in3, HIGH);
+  digitalWrite(rightFront_in1, HIGH);
+  digitalWrite(rightFront_in2, LOW);
+  digitalWrite(leftBack_in1, HIGH);
+  digitalWrite(leftBack_in2, LOW);
+  digitalWrite(rightBack_in4, LOW);
+  digitalWrite(rightBack_in3, HIGH);
+
+  analogWrite(leftFront_enB, 255);
+  analogWrite(rightFront_enA, 255);
+  analogWrite(leftBack_enA, 255);
+  analogWrite(rightBack_enB, 255);
+}
+
+// Right front and left back wheels move forward, left front and right back wheels move backward **NOT TESTED
+void goLeft() {
+  digitalWrite(leftFront_in4, HIGH);
+  digitalWrite(leftFront_in3, LOW);
+  digitalWrite(rightFront_in1, LOW);
+  digitalWrite(rightFront_in2, HIGH);
+  digitalWrite(leftBack_in1, LOW);
+  digitalWrite(leftBack_in2, HIGH);
+  digitalWrite(rightBack_in4, HIGH);
+  digitalWrite(rightBack_in3, LOW);
+
+  analogWrite(leftFront_enB, 255);
+  analogWrite(rightFront_enA, 255);
+  analogWrite(leftBack_enA, 255);
+  analogWrite(rightBack_enB, 255);
+}
+
+// Right front and left back wheels move forward **NOT TESTED
+void goForwardRight() {
+  digitalWrite(rightFront_in1, LOW);
+  digitalWrite(rightFront_in2, HIGH);
+  digitalWrite(leftBack_in1, LOW);
+  digitalWrite(leftBack_in2, HIGH);
+
+  analogWrite(leftFront_enB, 0);
+  analogWrite(rightFront_enA, 255);
+  analogWrite(leftBack_enA, 255);
+  analogWrite(rightBack_enB, 0);
+}
+
+// Left front and right back wheels move forward **NOT TESTED
+void goForwardLeft() {
+  digitalWrite(leftFront_in4, LOW);
+  digitalWrite(leftFront_in3, HIGH);
+  digitalWrite(rightBack_in4, LOW);
+  digitalWrite(rightBack_in3, HIGH);
+
+  analogWrite(leftFront_enB, 255);
+  analogWrite(rightFront_enA, 0);
+  analogWrite(leftBack_enA, 0);
+  analogWrite(rightBack_enB, 255);
+}
+
+// Left front and right back wheels move backward **NOT TESTED
+void goBackwardRight() {
+  digitalWrite(leftFront_in4, HIGH);
+  digitalWrite(leftFront_in3, LOW);
+  digitalWrite(rightBack_in4, HIGH);
+  digitalWrite(rightBack_in3, LOW);
+
+  analogWrite(leftFront_enB, 255);
+  analogWrite(rightFront_enA, 0);
+  analogWrite(leftBack_enA, 0);
+  analogWrite(rightBack_enB, 255);
 }
 
 // Right front and left back wheels move backward **NOT TESTED
